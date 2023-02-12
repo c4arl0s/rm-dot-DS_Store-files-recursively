87596d4096db92bda1465264276639974822b974
c4arl0s <c.santiago.cruz@gmail.com>
2023-02-11 21:58:46 -0600

    Remove useless variable

--- rmDotDSStoreScript.sh
+++ rmDotDSStoreScript.sh
@@ -2,11 +2,9 @@
 # it finds all .DS_Store files in the current directory and deletes them
 
 echo "finding .DS_Store files ..."
-counter=0
 find . -name "*" -type f | sort -n -r | grep ".DS_Store"
 find . -name "*" -type f | sort -n -r | grep ".DS_Store" | while read dsStoreFile
 do
-    let counter++
-    echo "$counter deleting ... $dsStoreFile"
+    echo "$COUNTER deleting ... $dsStoreFile"
     rm $dsStoreFile
 done
