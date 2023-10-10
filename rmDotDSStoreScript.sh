dd0b623734191d90d86e4ef83b06324383e06aeb
c4arl0s <c.santiago.cruz@gmail.com>
2023-10-09 21:46:51 -0600

    Fix COUNTER variable

--- rmDotDSStoreScript.sh
+++ rmDotDSStoreScript.sh
@@ -2,9 +2,11 @@
 # it finds all .DS_Store files in the current directory and deletes them
 
 echo "finding .DS_Store files ..."
+COUNTER=0
 find . -name "*" -type f | sort -n -r | grep ".DS_Store"
 find . -name "*" -type f | sort -n -r | grep ".DS_Store" | while read dsStoreFile
 do
+    let COUNTER=$COUNTER+1
     echo "$COUNTER deleting ... $dsStoreFile"
     rm $dsStoreFile
 done
